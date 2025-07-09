5a4dfc98748304bea80c94c79106964fb3858509
ethawn234 <ethawn234@gmail.com>
2025-07-09 15:06:05 -0500

    psuedo-code the solution

--- octokit.js
+++ octokit.js
@@ -13,55 +13,38 @@ const res = await octokit.request('GET /user', {
 });
 
 console.log(res.status)
-getChangedFiles('ethawn234', 'gh-rest-demo')
 
-export async function getChangedFiles({owner, repo, pullNumber}) {
-  let filesChanged = []
-
-  try {
-    const iterator = octokit.paginate.iterator("GET /repos/{owner}/{repo}/pulls/{pull_number}/files", {
-      owner: owner,
-      repo: repo,
-      pull_number: pullNumber,
-      per_page: 100,
-      headers: {
-        "x-github-api-version": "2022-11-28",
-      },
-    });
-
-    for await (const {data} of iterator) {
-      filesChanged = [...filesChanged, ...data.map(fileData => fileData.filename)];
-    }
-  } catch (error) {
-    if (error.response) {
-      console.error(`Error! Status: ${error.response.status}. Message: ${error.response.data.message}`)
-    }
-    console.error(error)
-  }
-
-  return filesChanged
+const repos = async function getRepos(){
+  // takes single arg: list of repos
+  // fetch all repos & branches in list
+  // return an object where k,v is { repo: branches[] }
 }
 
-async function commentIfDataFilesChanged({owner, repo, pullNumber}) {
-  const changedFiles = await getChangedFiles({owner, repo, pullNumber});
+async function _validateBranchProtection(){
+  // takes repo & branch and returns boolean for branch protection status
+}
 
-  const filePathRegex = new RegExp(/\/data\//, "i");
-  if (!changedFiles.some(fileName => filePathRegex.test(fileName))) {
-    return;
-  }
-
+async function updateFile(owner, repo, path){
+  // updater function
+  // args: repo, branch, path/to/file
+  // updates target file if unprotected
+  // updates target file and creates PR if branch is protected
   try {
-    const {data: comment} = await octokit.request("POST /repos/{owner}/{repo}/issues/{issue_number}/comments", {
-      owner: owner,
-      repo: repo,
-      issue_number: pullNumber,
-      body: `It looks like you changed a data file. These files are auto-generated. \n\nYou must revert any changes to data files before your pull request will be reviewed.`,
-      headers: {
-        "x-github-api-version": "2022-11-28",
+    await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', {
+      owner: 'OWNER',
+      repo: 'REPO',
+      path: 'PATH',
+      message: 'my commit message',
+      committer: {
+        name: 'Monalisa Octocat',
+        email: 'octocat@github.com'
       },
+      content: 'bXkgbmV3IGZpbGUgY29udGVudHM=',
+      sha: '95b966ae1c166bd92f8ae7d1c313e738c731dfc3',
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
     });
-
-    return comment.html_url;
   } catch (error) {
     if (error.response) {
       console.error(`Error! Status: ${error.response.status}. Message: ${error.response.data.message}`)
@@ -70,4 +53,13 @@ async function commentIfDataFilesChanged({owner, repo, pullNumber}) {
   }
 }
 
-await commentIfDataFilesChanged({owner: "github", repo: "docs", pullNumber: 191});
+const updateAFile = await updateFile('ethawn234', 'gh-rest-demo', );
+console.log(updateAFile)
+
+async function entryPoint(){
+  // routing function
+  // call getRepos to list all repos that need code quality flag
+  // iterates through list of objects keyed by repoName. The value is a list of all branches in the repo
+  // call _validateBranchProtection to determine branch protection
+  // call updateFile
+}

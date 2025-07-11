83d88a94711da94d7906da0d706a2f54a810d4bb
ethawn234 <ethawn234@gmail.com>
2025-07-11 10:17:17 -0500

    working implementation to update test.yml for all unprotected branches

--- octokit.js
+++ octokit.js
@@ -3,10 +3,12 @@ import yaml, { JSON_SCHEMA } from 'js-yaml';
 import dotenv from 'dotenv';
 dotenv.config()
 
+// instantiate octokit
 const octokit = new Octokit({
   auth: process.env.TOKEN
 })
 
+// authenticate to GitHub
 async function auth() {
   
   const token = await octokit.request('GET /user', {
@@ -17,6 +19,8 @@ async function auth() {
   return token;
 }
 
+
+// refactor to get single branch
 async function getBranches(owner, repoName){
   const branches = await octokit.request('GET /repos/{owner}/{repo}/branches', {
     owner: owner,
@@ -28,27 +32,18 @@ async function getBranches(owner, repoName){
   return branches;
 }
 
-async function _getFileSha(owner, repo, fileName) {
-  const getFileSha = await octokit.request('GET /repos/{owner}/{repo}/contents/.github/workflows/{file_sha}', { // get file sha
+async function _getFileContents(owner, repo, branch, fileName) {
+  // get repository content by branch
+  const getFileContent = await octokit.request('GET /repos/{owner}/{repo}/contents/.github/workflows/{file_sha}', {
     owner: owner,
     repo: repo,
+    ref: branch,
     file_sha: fileName,
     headers: {
       'X-GitHub-Api-Version': '2022-11-28'
     }
   });
-  const fileSha = getFileSha.data.sha;
-
-  const response = await octokit.request('GET /repos/{owner}/{repo}/git/blobs/{file_sha}', { // 
-    owner: owner,
-    repo: repo,
-    file_sha: fileSha,
-    headers: {
-      'X-GitHub-Api-Version': '2022-11-28'
-    }
-  })
-  // console.log('getFileShaRes: ', response)
-  return response;
+  return getFileContent;
 }
 
 // Path parameters
@@ -64,21 +59,16 @@ async function _getFileSha(owner, repo, fileName) {
 async function updateFile(owner, repo, path, branch, fileContents, fileSHA){
   try {
     const data = yaml.load(fileContents, JSON_SCHEMA);
-    // console.log('data:', JSON.stringify(data, null, 2));
-    
     data.jobs.validate_get_last_build_wf.steps.push({ name: 'My Extra Step', run: 'echo Hello' });
-
-    const modified = yaml.dump(data); 
-    // console.log('stringed modified: ', JSON.stringify(modified))
-    // console.log('modified: ', modified)
+    const modified = yaml.dump(data);
     const updatedContent = Buffer.from(modified, 'utf8').toString('base64');
 
-    console.log('updatedContent: ', atob(updatedContent))
-
+    // unprotected files updated in all branches; make this request idempotent -> this currently appends
     const pushChange = await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', {
       owner: owner,
       repo: repo,
       path: path,
+      branch: branch,
       message: 'add line to test.yml',
       committer: {
         name: owner,
@@ -91,7 +81,7 @@ async function updateFile(owner, repo, path, branch, fileContents, fileSHA){
       }
     });
 
-    console.log('pushChange: ', pushChange)
+    // console.log('pushChange: ', pushChange)
   } catch (error) {
     if (error.response) {
       console.error(`Error! Status: ${error.response.status}. Message: ${error.response.data.message}`)
@@ -100,6 +90,7 @@ async function updateFile(owner, repo, path, branch, fileContents, fileSHA){
   }
 }
 
+// entry point
 async function main(){
   try {
     // auth
@@ -110,22 +101,38 @@ async function main(){
       console.log(`Authentication success for ${authStatus.data.login}. Status: ${authStatus.status}`)
     }
 
+    // Steps
+    // from repoNames[], fetch each repo
+    // for each repo, fetch all branches
+
+    // for all protected branches, update yml & create PR
+    // for all unprotected branches, update yml & push
+      // target file is identified via its sha
+        // currently, 1st modification succeeds and all subsequent reqs fail with 409: non-matching sha - WHY?!!
+
     repos.forEach(async (repo, i) => {
-      const response = await getBranches(owner, repo); // gets all branches
+      const branches = await getBranches(owner, repo); // gets all branches; response.data[{br},..]
+      // [{
+      //   name: 'abcd',
+      //   commit: [Object],
+      //   protected: false,
+      //   protection: [Object],
+      //   protection_url: 'https://api.github.com/repos/ethawn234/gha-docker/branches/abcd/protection'
+      // },]
       
-      if (response.status == 200 && response.data.length > 0){
-        response.data.forEach(async branch => { // loop over all branches in current repo
+      if (branches.status == 200 && branches.data.length > 0){
+        branches.data.forEach(async branch => {
           const isProtected = branch.protected;
           const branchName = branch.name;
           
           if(isProtected == false){
             // br is unprotected; add flag directly
-            const fileContentsResponse = await _getFileSha(owner, repo, 'test.yml'); // replace file with target file
+            const fileContentsResponse = await _getFileContents(owner, repo, branchName, 'test.yml'); // replace file with target file
             const fileSHA = fileContentsResponse.data.sha;
-            const file = Buffer.from(fileContentsResponse.data.content, 'base64')//.toString('utf8');
+            const file = Buffer.from(fileContentsResponse.data.content, 'base64').toString('utf8');
 
-            // console.log(`${repo}-${i}-${branchName}-${fileSHA} type: ${typeof file} file: \n${file}\n`)  
-            updateFile(owner, repo, path, branchName, file, fileSHA) // current error
+            // console.log(`${repo}-${i}-${branchName}-${fileSHA} type: ${typeof file} file: \n${file}\n`)
+            updateFile(owner, repo, path, branchName, file, fileSHA) 
           } else {
             // update file and create PR
             console.log('protected br')
